8162c57d13e270969d0c31b30cf40df6f20dda99
Matthias Pfeil <matthias.pfeil@wwu.de>
2019-11-15 14:06:41 +0100

    run eslint on yarn test

--- example.js
+++ example.js
@@ -1,20 +1,20 @@
 const luqs = require('.')
 
-// luqs()
-//   .then(response => {
-//     console.log(response)
-//   })
-//   .catch(error => {
-//     console.log(error)
-//   })
+luqs()
+  .then(response => {
+    console.log(response)
+  })
+  .catch(error => {
+    console.log(error)
+  })
 
-// luqs.station('unna')
-//   .then(response => {
-//     console.log(response)
-//   })
-//   .catch(error => {
-//     console.log(error)
-//   })
+luqs.station('unna')
+  .then(response => {
+    console.log(response)
+  })
+  .catch(error => {
+    console.log(error)
+  })
 
 luqs.aktuell()
   .then(response => {
